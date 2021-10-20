1278cc0197e491b77be5eea483bec95b8d0d982d
Muhammad Mustafa Dhedhy <58875026+mustafadhedhy@users.noreply.github.com>
2021-10-20 21:05:29 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string buff; ;
+            FileStream F = new FileStream("cList.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(F);
+            buff = Console.ReadLine();
+
+            sw.WriteLine(buff);
+            //this is an edit
+            buff = sw.ReadLine();
+            Console.Write(buff);
+            sw.Close();
+            F.Close();
+        }
+    }
+}
